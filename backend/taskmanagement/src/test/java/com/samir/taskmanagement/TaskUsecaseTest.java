b0ecf5f13deda36abc2ea05737ce5ca3b6d1c2f6
Samir <samir@itchallenge.fr>
2024-10-04 10:40:30 +0100

    feat: spring backend initial commit

--- backend/taskmanagement/src/test/java/com/samir/taskmanagement/TaskUsecaseTest.java
+++ backend/taskmanagement/src/test/java/com/samir/taskmanagement/TaskUsecaseTest.java
@@ -1,4 +1,19 @@
 package com.samir.taskmanagement;
 
+import com.samir.taskmanagement.domain.entities.Task;
+import org.junit.jupiter.api.Test;
+
 public class TaskUsecaseTest {
+
+    @Test()
+    public void testTaskUsecase() {
+        // Arrange
+        var task = new Task();
+
+        // Act
+
+        // Assert
+
+
+    }
 }
